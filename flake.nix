5e522f28f3979d31f4b017ed0b2c805a801076de
Shringe <git@nsaria.com>
2025-06-14 20:47:30 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        nf = pkgs.rustPlatform.buildRustPackage {
+          pname = "nf";
+          version = "0.1";
+
+          cargoLock.lockFile = ./Cargo.lock;
+          src = pkgs.lib.cleanSource ./.;
+        };
+      in {
+        packages.default = nf;
+
+        devshells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            git
+            cargo
+          ];
+        };
+      }
+    );
+}

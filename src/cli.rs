e46bc1a3e79042bed9ea54a3ef4aa89a06a1d87a
Shringe <dashingkoso@gmail.com>
2025-06-22 00:52:22 -0500

    improved documentation

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,8 @@ use clap::Parser;
 use crate::mode::Mode;
 
 /// Simple program inspired by nix-helper that allows for fancy nix command expansions.
+/// To use certain commands you first need to generate a default configuration using:
+/// `nf config --generate-default`
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {

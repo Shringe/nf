632781eb9b2c03be6b8fbe714b97f7128bc1c339
Shringe <git@nsaria.com>
2025-06-22 00:52:38 -0500

    version 1.1 -> 1.2

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
 
         nf = pkgs.rustPlatform.buildRustPackage {
           pname = "nf";
-          version = "1.1";
+          version = "1.2";
 
           cargoLock.lockFile = ./Cargo.lock;
           src = pkgs.lib.cleanSource ./.;

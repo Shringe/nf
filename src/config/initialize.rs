86c88038edbe51b313cce852e629d0f0577a8cab
Shringe <dashingkoso@gmail.com>
2025-06-22 20:24:18 -0500

    moved error handling up

--- src/config/initialize.rs
+++ src/config/initialize.rs
@@ -1,25 +1,28 @@
-use std::{fs, path::PathBuf};
+use std::{fs, io::Result, path::PathBuf};
 
 use include_dir::{Dir, DirEntry, File};
 
 /// Initializes a default config file
-pub fn init_file(file: &File<'_>, to: &PathBuf) {
+pub fn init_file(file: &File<'_>, to: &PathBuf) -> Result<()> {
     let file_name = file.path().file_name().unwrap();
     let file_path = to.join(file_name);
 
-    fs::write(&file_path, file.contents()).expect("Couldn't create config file!");
+    fs::write(&file_path, file.contents())?;
+    Ok(())
 }
 
 /// Recursively initializes the default config
-pub fn init_recursive(from: &Dir<'_>, to: &PathBuf) {
+pub fn init_recursive(from: &Dir<'_>, to: &PathBuf) -> Result<()> {
     for entry in from.entries() {
         match entry {
             DirEntry::Dir(dir) => {
-                let to = to.join(dir.path().file_name().expect("Couldn't get directory name!"));
-                fs::create_dir(&to).expect("Couldn't create directory!");
-                init_recursive(dir, &to);
+                let to = to.join(dir.path().file_name().unwrap());
+                fs::create_dir(&to)?;
+                init_recursive(dir, &to)?;
             },
-            DirEntry::File(file) => init_file(&file, &to),
+            DirEntry::File(file) => init_file(&file, &to)?,
         }
     }
+
+    Ok(())
 }

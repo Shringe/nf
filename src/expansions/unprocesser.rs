8d9a2edaaf7459ab4421e84af909c5ec35e414e9
Shringe <dashingkoso@gmail.com>
2025-06-25 14:09:27 -0500

    cleaned code

--- src/expansions/unprocesser.rs
+++ src/expansions/unprocesser.rs
@@ -4,31 +4,6 @@ use crate::cli::Actionable;
 
 use super::cmd;
 
-/// Strips unnecessary -- between nixpkgs and to_command arguements. For example:
-/// nixpkgs#hello -- to_command -> nixpkgs#hello to_command
-fn strip_delimiter(args: &mut Vec<String>) {
-    if let Some(i) = cmd::index_of_flag(args, "--") {
-        if args.len() > i + 1 && args[i - 1].starts_with("nixpkgs#") {
-            args.remove(i);
-        }
-    }
-}
-
-/// Finds the first arg prefixed with nixpkgs# and removes the prefix
-fn unformat_nixpkg(args: &mut Vec<String>) {
-    for a in args.iter_mut() {
-        if let Some(stripped) = a.strip_prefix("nixpkgs#") {
-            *a = stripped.to_string();
-            break;
-        }
-    }
-}
-
-/// Replaces the first arguement (nix) with nf
-fn replace_nix(args: &mut Vec<String>) {
-    args[0] = "nf".to_string()
-}
-
 #[derive(Debug, Args)]
 pub struct UnProcesser {
     #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
@@ -37,7 +12,9 @@ pub struct UnProcesser {
 
 impl Actionable for UnProcesser {
     fn perform(&self, debug: bool) {
-        todo!()
+        if debug {} // Gets rid of the warning since debug is not used here
+        let expanded = self.unprocess();
+        println!("> {}", cmd::to_string(&expanded));
     }
 }
 
@@ -56,16 +33,7 @@ impl UnProcesser {
         out
     }
 
-    fn get_nixpkg_index(&self) -> Option<usize> {
-        for (i, a) in self.args.iter().enumerate() {
-            if a.starts_with("nixpkgs#") {
-                return Some(i);
-            }
-        }
-        None
-    }
-
-    fn get_args(&self) -> (Vec<String>, Vec<String>) {
+    fn get_args(&self) -> Vec<String> {
         let mut nix_args = Vec::new();
         let mut program_args = Vec::new();
 
@@ -86,38 +54,34 @@ impl UnProcesser {
 
         }
 
-        (nix_args, program_args)
+        if !nix_args.is_empty() {
+            nix_args.push("--".to_string());
+        }
+
+        let mut out = Vec::with_capacity(nix_args.len() + program_args.len());
+        out.extend(nix_args);
+        out.extend(program_args);
+        out
     }
 
-    fn get_args_to_program(&self, delimiter_index: Option<usize>) -> Vec<String> {
-        if let Some(delimiter) = delimiter_index {
-            self.args[delimiter+1..].to_owned()
-        } else {
-            Vec::new()
+    fn get_pkg(&self) -> Option<String> {
+        for a in self.args.iter() {
+            if let Some(stripped) = a.strip_prefix("nixpkgs#") {
+                return Some(stripped.to_string());
+            }
         }
+
+        None
     }
 
     fn run(&self) -> Vec<String> {
-        let prefix = cmd::from_string("nf run");
-        let nixpkg_index = self.get_nixpkg_index();
+        let mut out = cmd::from_string("nf run");
 
-        let args = self.get_args(); 
-        let nix_args = args.0;
-        let program_args = args.1;
-
-        let mut out = prefix;
-        if let Some(nixpkg) = nixpkg_index {
-            out.push(self.args[nixpkg].strip_prefix("nixpkgs#").unwrap().to_owned());
+        if let Some(pkg) = self.get_pkg() {
+            out.push(pkg);
         }
 
-        let needs_delimiter = !nix_args.is_empty();
-        out.extend(nix_args);
-        if needs_delimiter {
-            out.push("--".to_string());
-        }
-
-        out.extend(program_args);
-
+        out.extend(self.get_args());
         out
     }
 
@@ -145,39 +109,12 @@ mod tests {
     }
 
     #[test]
-    fn get_nixpkg_index() {
-        let up = UnProcesser { args: cmd::from_string("nix run nixpkgs#eza") };
-        assert_eq!(up.get_nixpkg_index(), Some(2));
-        let up = UnProcesser { args: cmd::from_string("nix run") };
-        assert_eq!(up.get_nixpkg_index(), None);
-    }
-
-    #[test]
-    fn get_args_to_nix() {
+    fn get_args() {
         let map = HashMap::from([
             ("nix run nixpkgs#eza", ""),
-            ("nix run nixpkgs#eza to_nix_after", "to_nix_after"),
-            ("nix run to_nix_before nixpkgs#eza to_nix_after", "to_nix_before to_nix_after"),
-            ("nix run to_nix_before nixpkgs#eza to_nix_after -- to_program", "to_nix_before to_nix_after "),
-            ("nix run nixpkgs#eza -- to_program_one to_program_two", ""),
-        ]);
-
-        for (k, v) in map {
-            let input = cmd::from_string(k);
-            let expected = cmd::from_string(v);
-            let up = UnProcesser { args: input.clone() };
-            let out = up.get_args().0;
-            validate_processer_test(&input, &expected, &out);
-        }
-    }
-
-    #[test]
-    fn get_args_to_program() {
-        let map = HashMap::from([
-            ("nix run nixpkgs#eza", ""),
-            ("nix run nixpkgs#eza to_nix_after", ""),
-            ("nix run to_nix_before nixpkgs#eza to_nix_after", ""),
-            ("nix run to_nix_before nixpkgs#eza to_nix_after -- to_program", "to_program"),
+            ("nix run nixpkgs#eza to_nix_after", "to_nix_after --"),
+            ("nix run to_nix_before nixpkgs#eza to_nix_after", "to_nix_before to_nix_after --"),
+            ("nix run to_nix_before nixpkgs#eza to_nix_after -- to_program", "to_nix_before to_nix_after -- to_program"),
             ("nix run nixpkgs#eza -- to_program_one to_program_two", "to_program_one to_program_two"),
         ]);
 
@@ -185,36 +122,9 @@ mod tests {
             let input = cmd::from_string(k);
             let expected = cmd::from_string(v);
             let up = UnProcesser { args: input.clone() };
-            let out = up.get_args().1;
+            let out = up.get_args();
             validate_processer_test(&input, &expected, &out);
         }
-    }
-
-    #[test]
-    fn replace_nix() {
-        let input    = cmd::from_string("nix run");
-        let expected = cmd::from_string("nf run");
-        let mut out = input.clone();
-        super::replace_nix(&mut out);
-        validate_processer_test(&input, &expected, &out);
-    }
-
-    #[test]
-    fn strip_delimiter() {
-        let input    = cmd::from_string("nix run nixpkgs#hello -- to_command");
-        let expected = cmd::from_string("nix run nixpkgs#hello to_command");
-        let mut out = input.clone();
-        super::strip_delimiter(&mut out);
-        validate_processer_test(&input, &expected, &out);
-    }
-
-    #[test]
-    fn unformat_nixpkg() {
-        let input    = cmd::from_string("nix run nixpkgs#hello");
-        let expected = cmd::from_string("nix run hello");
-        let mut out = input.clone();
-        super::unformat_nixpkg(&mut out);
-        validate_processer_test(&input, &expected, &out);
     }
 
     #[test]

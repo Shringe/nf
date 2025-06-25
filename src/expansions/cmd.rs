a69a78c34b54e5eeb93c189f831eee3db490ecb9
Shringe <git@nsaria.com>
2025-06-25 14:09:27 -0500

    cleaned code

--- src/expansions/cmd.rs
+++ src/expansions/cmd.rs
@@ -1,10 +1,11 @@
 use std::{os::unix::process::CommandExt, process::Command};
 
 /// Asserts that input = expected with a pretty failure message
+#[cfg(test)]
 pub fn validate_processer_test(input: &Vec<String>, expected: &Vec<String>, out: &Vec<String>) {
     assert_eq!(
-        out,
         expected,
+        out,
         "\n    Input: {}\n Expected: {}\n   Actual: {}",
         to_string(input), to_string(expected), to_string(out)
     );
@@ -18,24 +19,9 @@ pub fn from_string<S: AsRef<str>>(args: S) -> Vec<String> {
     args.as_ref().split_whitespace().map(|s| s.to_string()).collect()
 }
 
-pub fn from_vec<S: Into<String>>(args: Vec<S>) -> Vec<String> {
-    args.into_iter().map(|s| s.into()).collect()
-}
-
 /// Check if args contain a specific flag
 pub fn contains_flag(args: &Vec<String>, flag: &str) -> bool {
     args.iter().any(|arg| arg == flag)
-}
-
-/// Finds the index of a flag if it exists
-pub fn index_of_flag(args: &Vec<String>, flag: &str) -> Option<usize> {
-    for (i, arg) in args.iter().enumerate() {
-        if arg == flag {
-            return Some(i);
-        }
-    }
-
-    None
 }
 
 /// Replaces the current process with a new one.
